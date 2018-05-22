3eea763b468aad1a90ed1b6d2a61f408491eb441
Christopher Stull <chris.stull@gmail.com>
2018-05-22 18:58:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function accessElementInArray(arr,index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(arr){
-  
+  shift.arr
   return arr
 }
 
@@ -30,6 +30,4 @@ function removeElementFromBeginningOfArray(arr){
   return cut
 }
 
-function destructivelyRemoveElementFromEndOfArray(arr){
-  
-}
+function destructivelyRemoveElementFromEndOfArray(arr)
