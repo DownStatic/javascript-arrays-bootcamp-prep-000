97c22be42d6ccf0d167c84313f7b0935f12bdd70
Christopher Stull <chris.stull@gmail.com>
2018-05-22 18:43:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,23 +1,2 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
-var grilledCheeseIngredients = [
-  'bread',
-  'mild cheese',
-  'sharp cheese',
-  'butter',
-  'tomato',
-  'garlic'
-]
-
-var tomatoSauceIngredients = [
-  'tomato',
-  'garlic',
-  'olive oil',
-  'basil',
-  'oregano'
-]
